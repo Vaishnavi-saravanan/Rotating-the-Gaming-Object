682ba920cceece758c7143a3b4728a3a9862709e
Vaishnavi-Saravanan <vaishnavisaravanan2729@gmail.com>
2024-03-06 20:45:51 +0530

    Initial commit

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -13,6 +13,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // type the code here
+        transform.RotateAround(Vector.Up,Vector3.right,80*Time.deltaTime)
     }
 }
